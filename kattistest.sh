830a3d3717b9390c50d8bfb2cc82a9d559ab2f98
Cormac Smartt <cormacsmartt@gmail.com>
2017-07-25 20:03:21 +0100

    Suppress output & clean up before & after

--- kattistest.sh
+++ kattistest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 problem=$(basename "$PWD")
-wget --progress=bar -q -O samples.zip "https://open.kattis.com/problems/"$problem"/file/statement/samples.zip" && dtrx samples.zip
+rm -rf ./samples* && wget -q -O samples.zip "https://open.kattis.com/problems/"$problem"/file/statement/samples.zip" && dtrx samples.zip
 for i in ./samples/*.in;
 	do 
 		./$problem < "$i" > "${i%.*}".out;
-		if ! diff -q "${i%.*}".out "${i%.*}".ans; then
+		if ! diff -q "${i%.*}".out "${i%.*}".ans &>/dev/null; then
 			echo "Test `basename "${i%.*}"` failed."
 		else
 			echo "Test `basename "${i%.*}"` passed."	
