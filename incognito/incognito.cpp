1c1caf42efa02f2ebfbe5e8858c69e8d8d0a85fd
Cormac Smartt <cormacsmartt@gmail.com>
2017-09-01 21:47:31 +0100

    incognito

---
+++ incognito/incognito.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+	int cases;
+	cin >> cases;
+	for(int i = 0; i < cases; i++) {
+		int attrs;
+		cin >> attrs;
+		map<string, int> categories;
+		for(int j =0; j < attrs; j++) {
+			string id, type;
+			cin >> id >> type;
+			categories.insert(pair<string, int>(type, categories[type] += 1));
+		}
+		int sum = 1;
+		for(auto& x : categories) {
+			sum += x.second*sum;
+		}
+		cout << sum - 1 << endl;
+	}
+
+	return 0;
+}
